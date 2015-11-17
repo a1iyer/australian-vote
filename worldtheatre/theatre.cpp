f6a4937764ba1c91244a55f14b9f5500e0059ad3
Anoop Iyer <a1.iyer@samsung.com>
2015-11-17 15:37:30 -0600

    Cleaned up 131C

--- worldtheatre/theatre.cpp
+++ worldtheatre/theatre.cpp
@@ -1,11 +1,10 @@
-// theatre.c
+// theatre.cpp
 // Anoop Iyer, Nov 2015
 // http://codeforces.com/problemset/problem/131/C/
 
 #include <iostream>
 
 using namespace std;
-
 using num_t = long long;
 
 num_t C (num_t n, num_t k)
@@ -21,16 +20,11 @@ num_t C (num_t n, num_t k)
 
 int main (int argc, char * argv[])
 {
-    num_t n=0, m=0, t=0;
-    num_t result=0;
+    num_t n=0, m=0, t=0, result=0;
 
     cin >> n >> m >> t;
-
-    num_t min_girls = max<num_t> (1, t - n);
-    num_t min_boys = max<num_t> (4, t - m);
-    for (num_t boys = min_boys; boys <= t - min_girls; boys++) {
-        result += C (n, boys) * C (m, t - boys);
-    }
+    for (num_t b = max<num_t> (4, t-m); b <= t - max<num_t> (1, t-n); b++) 
+        result += C (n, b) * C (m, t - b);
     cout << result << '\n';
     return 0;
 }
